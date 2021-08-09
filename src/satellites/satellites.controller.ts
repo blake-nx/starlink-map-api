2e826ac7d1448d9214e00c57836808150a481e06
Blake Enloe <blakewenloe@yahoo.com>
2021-08-08 20:09:54 -0500

    added route for getting satellites above user

--- src/satellites/satellites.controller.ts
+++ src/satellites/satellites.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { SatellitesService } from './satellites.service';
 
 @Controller('satellites')
 export class SatellitesController {
   constructor(private satellitesService: SatellitesService) {}
-  @Get('/above-me')
-  getAboveSatellites() {
-    return this.satellitesService.getAboveSatellites();
+  @Get('/above-me/:lat/:lng')
+  getAboveSatellites(@Param() params) {
+    return this.satellitesService.getAboveSatellites(params.lat, params.lng);
   }
 }
--- src/satellites/satellites.service.ts
+++ src/satellites/satellites.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import dotenv from 'dotenv/config';
+const API_KEY = process.env.API_KEY;
 
 @Injectable()
 export class SatellitesService {
   private satellitesAbove = [];
-
-  public getAboveSatellites() {
-    return this.satellitesAbove;
+  fetchSatellites = async (lat, lng) => {
+    return await axios
+      .get(
+        `https://api.n2yo.com/rest/v1/satellite/above/${lat}/${lng}/0/14/52/&apiKey=${API_KEY}`,
+      )
+      .then((response) => {
+        return response.data === null
+          ? 'No satellites found'
+          : response.data.above;
+      });
+  };
+  public getAboveSatellites(lat, lng) {
+    return this.fetchSatellites(lat, lng);
   }
 }
