48fc6f74bbea45dee4e831d5f3669a5e0a562cde
Blake Enloe <blakewenloe@yahoo.com>
2021-08-08 22:43:30 -0500

    fixed dotenv import

--- src/satellites/satellites.service.ts
+++ src/satellites/satellites.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import dotenv from 'dotenv/config';
-const API_KEY = process.env.API_KEY;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 @Injectable()
 export class SatellitesService {
-  private satellitesAbove = [];
+  private apiKey = process.env.API_KEY;
   fetchSatellites = async (lat, lng) => {
     return await axios
       .get(
-        `https://api.n2yo.com/rest/v1/satellite/above/${lat}/${lng}/0/14/52/&apiKey=${API_KEY}`,
+        `https://api.n2yo.com/rest/v1/satellite/above/${lat}/${lng}/0/70/52/&apiKey=${this.apiKey}`,
       )
       .then((response) => {
         return response.data === null
