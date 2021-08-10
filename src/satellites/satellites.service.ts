fe1527d7616a38c8a57360bc1db54ab28b1f902e
Blake Enloe <blakewenloe@yahoo.com>
2021-08-09 19:29:32 -0500

    changed conditional to less than 1000 requests

--- src/satellites/satellites.service.ts
+++ src/satellites/satellites.service.ts
@@ -12,7 +12,7 @@ export class SatellitesService {
         `https://api.n2yo.com/rest/v1/satellite/above/${lat}/${lng}/0/70/52/&apiKey=${this.apiKey}`,
       )
       .then((response) => {
-        if (response.data.info.transactionscount <= 1000) {
+        if (response.data.info.transactionscount < 1000) {
           return response.data === null ? 'No satellites found' : response.data;
         } else {
           throw new Error('requests per hour exceeded');
